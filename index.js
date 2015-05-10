788bb9f2fe68035cf03fdc890955104bac3b9289
Chaoyang <cyjia@thoughtworks.com>
2015-05-10 18:31:34 +0800

    Can remove files.

---
+++ index.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var removeFile = require('broccoli-file-remover');
+
+module.exports = {
+  name: 'ember-cli-test-file-remover',
+
+  setupPreprocessorRegistry: function(type, registry) {
+    var options, appName;
+    if (registry && registry.app && registry.app.options) {
+      options = registry.app.options['test-file-remover'];
+      appName = registry.app.name;
+      if (options.files) {
+        console.log(appName);
+        options.files = options.files.map(function(f) {
+          return path.join(appName, "tests", f);
+        });
+        console.log(options.files);
+      }
+    }
+    registry.add('js', {
+      name: 'ember-cli-test-file-remover',
+      ext: 'js',
+      toTree: function(tree) {
+        if (options) {
+          return removeFile(tree, options);
+        }
+        else
+          return tree;
+      }
+    });
+  }
+};
